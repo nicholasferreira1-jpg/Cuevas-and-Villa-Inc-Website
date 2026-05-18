59e6fb9542d5c07d70270633b55ce616c20dab45
nicholasferreira1-jpg <nicholasferreira1@gmail.com>
2026-05-18 14:50:08 -0700

    Added config.py to read .env file

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,29 @@
-# Store environmental variables
+# Reads secrets from .env file
+
+import os
+from dotenv import load_dotenv
+
+# Load the .env file so Python can read it
+load_dotenv()
+
+# Flask Settings
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+# Database Settings
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Build the database connection string
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+# Email Settings
+
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
