bfeff2a1f40666b8469e619a4b9c4934af6d9cae
nicholasferreira1-jpg <nicholasferreira1@gmail.com>
2026-05-18 15:44:33 -0700

    Handled home page route

--- backend/routes/home.py
+++ backend/routes/home.py
@@ -1,1 +1,13 @@
 # Home page
+
+from flask import Blueprint, render_template
+
+# Create Blueprint
+
+home_bp = Blueprint("home", __name__)
+
+# Route
+
+@home_bp.route("/")
+def home():
+    return render_template("index.html")
