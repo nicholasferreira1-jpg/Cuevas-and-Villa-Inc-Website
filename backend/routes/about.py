7d00f8cec3cdb2ade94319ee40368fa82b78bbdc
nicholasferreira1-jpg <nicholasferreira1@gmail.com>
2026-05-18 15:47:14 -0700

    about.py - Handled about us page route

--- backend/routes/about.py
+++ backend/routes/about.py
@@ -1,1 +1,15 @@
 # About us page
+
+# Home page
+
+from flask import Blueprint, render_template
+
+# Create Blueprint
+
+about_bp = Blueprint("aboutus", __name__)
+
+# Route
+
+@about_bp.route("/aboutus")
+def about():
+    return render_template("about.html")
